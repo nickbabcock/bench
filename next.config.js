75b7d5f3c8c9b7e8c21d34ec54cf6b6aab5f6de5
Nick Babcock <nbabcock19@hotmail.com>
2023-01-31 23:49:08 +0000

    Add security headers

--- next.config.js
+++ next.config.js
@@ -1,3 +1,34 @@
+const isDev = process.env.NODE_ENV !== "production";
+
+const csp = `
+  default-src 'self';
+  script-src 'self' 'wasm-unsafe-eval' ${isDev ? "'unsafe-eval'" : ""};
+  style-src 'self' ${isDev ? "'unsafe-inline'" : ""};
+`;
+
+const securityHeaders = [
+  {
+    key: "X-Content-Type-Options",
+    value: "nosniff",
+  },
+  {
+    key: "X-Frame-Options",
+    value: "SAMEORIGIN",
+  },
+  {
+    key: "Strict-Transport-Security",
+    value: "max-age=8640000",
+  },
+  {
+    key: "Referrer-Policy",
+    value: "no-referrer",
+  },
+  {
+    key: "Content-Security-Policy",
+    value: csp.replace(/\s{2,}/g, " ").trim(),
+  },
+];
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
@@ -10,7 +41,15 @@ const nextConfig = {
       type: "asset/resource",
     });
     return config;
-  }
-}
+  },
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: securityHeaders,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
