1fc63af3695be66f8beaaf91bdaa393d3c2c01ca
Nick Babcock <nbabcock19@hotmail.com>
2026-06-16 07:16:43 -0500

    Update lz4 to frame encoder/decoder and bump lz4_flex to 0.13.1
    Frame format supports large inputs via streaming and includes checksums,
    unlike the block API which requires upfront knowledge of decompressed size.

--- packages/lz4/src/lib.rs
+++ packages/lz4/src/lib.rs
@@ -1,11 +1,17 @@
+use std::io::{Read, Write};
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
 pub fn decompress(data: &[u8]) -> Vec<u8> {
-    lz4_flex::decompress_size_prepended(data).unwrap()
+    let mut decoder = lz4_flex::frame::FrameDecoder::new(data);
+    let mut out = Vec::new();
+    decoder.read_to_end(&mut out).unwrap();
+    out
 }
 
 #[wasm_bindgen]
 pub fn compress(data: &[u8]) -> Vec<u8> {
-    lz4_flex::compress_prepend_size(data)
+    let mut encoder = lz4_flex::frame::FrameEncoder::new(Vec::new());
+    encoder.write_all(data).unwrap();
+    encoder.finish().unwrap()
 }
